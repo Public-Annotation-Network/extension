3a00a01ac81a030ee6e05a121bb82c0b8233d72b
jo-es <escherich.johannes+github@gmail.com>
2020-07-25 22:06:36 +0200

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,6 +1,6 @@
 const fetch = require('cross-fetch');
 
-
+const IPFS_URL = 'https://gateway.pinata.cloud/ipfs';
 const SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/public-annotation-network/subgraph';
 
 async function sendQuery (query) {
@@ -46,6 +46,7 @@ async function getAnnotations(first, skip) {
       {
         annotations(first: ${first}, skip: ${skip}) {
           cid
+          ref
           batchCID
         }
       }
@@ -59,7 +60,47 @@ async function getAnnotations(first, skip) {
   return result.data.annotations;
 }
 
+async function getAnnotationsByReference(first, skip, reference) {
+  const result = await sendQuery(
+    `
+      {
+        annotations(first: ${first}, skip: ${skip}, where: { ref_contains: "${reference}" }) {
+          cid
+          ref
+          batchCID
+        }
+      }
+    `
+  );
+
+  if (!result || !result.data || !result.data.annotations) {
+    throw new Error('No annotations found.');
+  }
+
+  return result.data.annotations;
+}
+
+async function getAnnotationData(cid) {
+  try {
+    const response = await fetch(
+      IPFS_URL,
+      { method: 'GET' }
+    );
+    const body = await response.json();
+
+    if (response.status !== 200 || (body.errors && body.errors.length > 0)) {
+      throw new Error(JSON.stringify(body.errors));
+    }
+
+    return body;
+  } catch (error) {
+    console.error(error);      
+    throw new Error('Could not query IPFS node: ' + error);
+  }
+}
+
 (async () => {
-  console.log(await getAnnotationBatches(10, 0));
-  console.log(await getAnnotations(10, 0));
+  // console.log(await getAnnotationBatches(10, 0));
+  // console.log(await getAnnotations(10, 0));
+  console.log(await getAnnotationsByReference(2, 0, 'joaosantos'));
 })();
